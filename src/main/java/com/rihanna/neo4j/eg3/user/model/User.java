52df93474abddafd74c5198342675e259139a71b
jrihanna <rihanna.zekri@gmail.com>
2021-01-13 00:04:54 +1100

    Enhancements

--- src/main/java/com/rihanna/neo4j/eg3/user/model/User.java
+++ src/main/java/com/rihanna/neo4j/eg3/user/model/User.java
@@ -3,6 +3,8 @@ package com.rihanna.neo4j.eg3.user.model;
 import java.util.List;
 
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,12 +14,12 @@ import lombok.Setter;
 @Setter
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private String userName;
 	private String password;
-	private List<String> likedIngredients;
-	private List<String> dislikedIngredients;
 
 }
